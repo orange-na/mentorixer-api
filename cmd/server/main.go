d8b62c9cf3250cb10bcbc13ff44fe77793cfa96b
orange-na <otuy1125abman@gmail.com>
2024-09-08 17:22:59 +0900

    環境変数の定義

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,25 @@
 package server
 
 import (
+	"fmt"
 	"main/internal/handler"
 	"main/internal/router"
 	"main/middleware"
 	"main/pkg/db"
 
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func Run() {
+    err := godotenv.Load()
+    if err != nil {
+		panic(err)
+    }
+
+
 	r := gin.Default()
 	db.Init()
 
@@ -23,5 +33,6 @@ func Run() {
 	router.FriendRoutes(r, friendHandler)
 	router.AuthRoutes(r, authHandler)
 
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+	r.Run(fmt.Sprintf(":%s", port))
 }
